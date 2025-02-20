ff730569f0ce951343907bc370f335206e629733
CreepyTeabag <sovunja1712@gmail.com>
2025-02-20 17:52:41 +0300

    Small fix

--- 12_favorite_places/util/api_key.js
+++
@@ -1,1 +0,0 @@
-export const API_KEY = "0a35995e99d64fca8bdb9eb56bf483cd";
--- 12_favorite_places/util/location.js
+++ 12_favorite_places/util/location.js
@@ -1,4 +1,4 @@
-import { API_KEY } from "./api_key";
+import { API_KEY } from "./api_key_2";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.geoapify.com/v1/staticmap?style=maptiler-3d&width=400&height=200&center=lonlat:${lng},${lat}&zoom=15&pitch=11&apiKey=${API_KEY}
